d3f220ef3b78752c215368d94a6a2092ba67d5e5
jmooradian <juniorbirdman52@gmail.com>
2021-06-30 19:55:39 -0500

    Added Bow, cleaned some things up.
    Added hot bar functionality:
    1 - sword
    2 - bow
    3 - empty
    
    Changed sword animation so that it is visible all the time when equipped.
    Changed Player.gd to only call sword.attack() when it is equipped.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,10 +2,12 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 onready var sword = $Sword
+onready var bow = $Bow
 
 func get_input():
-	if Input.is_action_pressed("click"):
-		sword.attack()
+	if Input.is_action_just_pressed("click"):
+		if PlayerAttributes.current_weapon == "sword":
+			sword.attack()
 	velocity = Vector2()
 	if Input.is_action_pressed('left'):
 		velocity.x -= 1
@@ -15,6 +17,28 @@ func get_input():
 		velocity.y -= 1
 	if Input.is_action_pressed('down'):
 		velocity.y += 1
+	
+	if Input.is_action_just_pressed("slot 1"):
+		#For now will be forced to sword
+		if PlayerAttributes.current_weapon != "sword":
+			PlayerAttributes.current_weapon = "sword"
+			sword.visible = true
+			bow.visible = false
+			print ("Current equipped weapon: ", PlayerAttributes.current_weapon)
+	if Input.is_action_just_pressed("slot 2"):
+		#For now will be forced to bow
+		if PlayerAttributes.current_weapon != "bow":
+			PlayerAttributes.current_weapon = "bow"
+			bow.visible = true
+			sword.visible = false
+			print ("Current equipped weapon: ", PlayerAttributes.current_weapon)
+	if Input.is_action_just_pressed("slot 3"):
+		if PlayerAttributes.current_weapon != "empty":
+			PlayerAttributes.current_weapon = "empty"
+			bow.visible = false
+			sword.visible = false
+			print ("Current equipped weapon: ", PlayerAttributes.current_weapon)
+			
 	velocity = velocity.normalized() * PlayerAttributes.speed
 	
 
--- Scripts/PlayerAttributes.gd
+++ Scripts/PlayerAttributes.gd
@@ -4,3 +4,6 @@ var health
 var armor = 10
 var swordDamage = 1
 var speed = 400
+
+
+var current_weapon = "empty"
