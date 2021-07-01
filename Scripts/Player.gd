672a6f38c94816ec8d9ea1ea0c46d0e79c27c0f2
hamza <hh0718@yahoo.com>
2021-06-30 23:46:36 -0500

    scroll wheel to change weapons

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,6 +4,9 @@ var velocity = Vector2()
 onready var sword = $Sword
 onready var bow = $Bow
 
+var weapons = ["empty", "sword", "bow"]
+var weaponsIndex = 0
+
 func get_input():
 	if Input.is_action_just_pressed("click"):
 		if PlayerAttributes.current_weapon == "sword":
@@ -18,6 +21,19 @@ func get_input():
 	if Input.is_action_pressed('down'):
 		velocity.y += 1
 	
+	if(Input.is_action_just_released("scrollWheelUp")):
+		weaponsIndex += 1
+		if(weaponsIndex >= weapons.size()):
+			weaponsIndex = 0
+		PlayerAttributes.current_weapon = weapons[weaponsIndex]
+		
+	if(Input.is_action_just_released("scrollWheelDown")):
+		weaponsIndex -= 1
+		if(weaponsIndex < 0):
+			weaponsIndex = weapons.size() - 1
+		PlayerAttributes.current_weapon = weapons[weaponsIndex]
+		
+		
 	if Input.is_action_just_pressed("slot 1"):
 		#For now will be forced to sword
 		if PlayerAttributes.current_weapon != "sword":
@@ -46,3 +62,12 @@ func _physics_process(delta):
 	get_input()
 	move_and_slide(velocity)
 	look_at(get_global_mouse_position())
+	if PlayerAttributes.current_weapon == "sword":
+		sword.visible = true
+		bow.visible = false
+	elif PlayerAttributes.current_weapon == "bow":
+		sword.visible = false
+		bow.visible = true
+	elif PlayerAttributes.current_weapon == "empty":
+		sword.visible = false
+		bow.visible = false
