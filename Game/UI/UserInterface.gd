30f5e2e116a8cf30d436a3670d619b9c6728a866
hamza <hh0718@yahoo.com>
2021-08-31 13:52:17 -0500

    game pauses when inv menu is open

--- Game/UI/UserInterface.gd
+++ Game/UI/UserInterface.gd
@@ -5,7 +5,11 @@ onready var inventory = get_node("Control/Inventory")
 onready var control = get_node("Control")
 
 
-
 func _process(delta):
 	if Input.is_action_just_released("inventoryMenu"):
 		control.visible = !control.visible
+		
+	if(control.visible):
+		get_tree().paused = true
+	else:
+		get_tree().paused = false
